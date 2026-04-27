71f056b963d38cd6d2b71e50a71beb885ed4a02f
Mahesh-88 <maheshbabu.m1@svcolleges.edu.in>
2026-04-27 22:34:27 +0530

    ffrvrf

--- One.java
+++ One.java
@@ -2,6 +2,6 @@ class One{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Welcome to Java Programming");
-        System.out.println("........");
+        System.out.println("coding is fun");
     }
 }
