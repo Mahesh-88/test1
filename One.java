24a6f08803adb64fec3b72495d581289753bbaa3
Mahesh-88 <maheshbabu.m1@svcolleges.edu.in>
2026-04-28 18:22:19 +0530

    Add sum calculation for two integers in One.java

--- One.java
+++ One.java
@@ -11,5 +11,10 @@ class One{
         System.out.println("Welcome to Java Programming");
         System.out.println("coding is fun");
 
+        //addition of two nums
+        int a=10;
+        int b=10;
+        int c=a+b;
+        System.out.println("the sum of a and b is "+c);
     }
 }
